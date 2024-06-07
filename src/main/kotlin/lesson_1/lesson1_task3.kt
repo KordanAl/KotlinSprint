4ae06baa7f0f2c2863b5f02ad5d6c0fd78369d84
KorolevDA <valnamekda@gmail.com>
2024-06-07 21:14:18 +0300

    KS-1-3 a space flight

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_1
+
+fun main () {
+
+    // решение задачи №3
+
+    // Год и время взлета Юрия Гагарина
+
+    val year: Short = 1961
+    var hour: Byte = 9
+    var minute: Byte = 7
+//    println("$year \n0$hour \n0$minute")
+
+    //Время посдаки
+
+    hour = 10
+    minute = 55
+    println("$hour:$minute")
+
+}
