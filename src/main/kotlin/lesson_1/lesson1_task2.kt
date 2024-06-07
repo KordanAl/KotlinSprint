91cdb460e8909296bc9dd8196556f1f2c2d74107
KorolevDA <valnamekda@gmail.com>
2024-06-07 17:39:05 +0300

    KS-1-2 add store employees

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_1
+
+fun main() {
+
+    // решение задачи №2
+
+    val numberOfOrders: Int = 75
+    val purchaseText: String = "Рады, что выбираете нас, спасибо за покупку!"
+    println("$numberOfOrders \n$purchaseText")
+
+    // количество сотрудников в нашем магазине
+
+    var storeEmployees = 2000
+//    println(storeEmployees)
+
+    // увольнение сотрудника
+
+    --storeEmployees
+    println(storeEmployees)
+
+
+}
