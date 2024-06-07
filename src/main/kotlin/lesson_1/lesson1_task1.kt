70b4c676075ef1562eb317e3275e913b8429f3e2
KorolevDA <valnamekda@gmail.com>
2024-06-07 17:17:21 +0300

    KS-1-1 create screen logic

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main() {
+
+    // решение задачи №1
+
+    val numberOfOrders: Int = 75
+    val purchaseText: String = "Рады, что выбираете нас, спасибо за покупку!"
+    println("$numberOfOrders \n$purchaseText")
+
+}
