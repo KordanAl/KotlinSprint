e6978c82da6b67b4d3c5a1536e48112cc644f48d
KorolevDA <valnamekda@gmail.com>
2024-06-08 15:58:57 +0300

    KS-1-5 time in space ( refactor )

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -2,8 +2,6 @@ package org.example.lesson_1
 
 fun main() {
 
-    // решение задачи №2
-
     val numberOfOrders: Int = 75
     val purchaseText: String = "Рады, что выбираете нас, спасибо за покупку!"
     println("$numberOfOrders \n$purchaseText")
