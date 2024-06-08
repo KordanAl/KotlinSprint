6931f01c838001e71ad9192fd7f88787b79a35a0
KorolevDA <valnamekda@gmail.com>
2024-06-08 16:13:10 +0300

    KS-1-5 time in space ( refactor )

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,15 +1,19 @@
 package org.example.lesson_1
 
-fun main() {
+const val SECONDS_IN_SPACE: Int = 5409
+const val SECONDS_IN_HOUR: Int = 3600
+const val SECONDS_IN_MINUTE: Int = 60
 
-    // Решение задачи №5
+fun main() {
 
     // Время Юрия Гагарина в космосе
 
-    val secondsInSpace: Int = 5409
-    val hours: Int = secondsInSpace / 3600
-    val minutes: Int = (secondsInSpace % 3600) / 60
-    val seconds: Byte = 9
-    println("0$hours:$minutes:0$seconds")
+    val hours: Int = SECONDS_IN_SPACE / SECONDS_IN_HOUR
+    val minutes: Int = (SECONDS_IN_SPACE % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val seconds: Int = SECONDS_IN_SPACE % SECONDS_IN_MINUTE
+
+    val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+
+    println(formattedTime)
 
 }
