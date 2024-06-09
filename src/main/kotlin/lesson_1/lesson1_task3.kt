ad7a57c35a6f440525141ca73515ea261b2b87a1
KorolevDA <valnamekda@gmail.com>
2024-06-09 10:01:06 +0300

    KS-1-3 time arithmetic operations (refactor 2)

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,16 +2,13 @@ package org.example.lesson_1
 
 fun main () {
 
-    // Год и время взлета Юрия Гагарина
-
     val year: Short = 1961
     var hour: String = "09"
     var minute: String = "07"
-//    println("$year")
-//    println("$hour")
-//    println("$minute")
 
-    //Время посдаки
+    println(year)
+    println(hour)
+    println(minute)
 
     hour = "10"
     minute = "55"
