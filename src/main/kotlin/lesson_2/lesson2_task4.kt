78ae3c4b4c2c9ff7b2b729b3ba108fcc60f9f127
KorolevDA <valnamekda@gmail.com>
2024-06-10 20:36:09 +0300

    KS-2-4 buff to material refactor

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,13 +1,14 @@
 package org.example.lesson_2
 
+const val ONE_HUNDRED_PERCENT = 100
 fun main () {
 
     val crystalMaterials = 7
     val ironMaterials = 11
     val buffPercentage = 20
 
-    val bonusCrystalMaterials = (crystalMaterials * buffPercentage / 100)
-    val bonusIronMaterials = (ironMaterials * buffPercentage / 100)
+    val bonusCrystalMaterials = (crystalMaterials * buffPercentage / ONE_HUNDRED_PERCENT)
+    val bonusIronMaterials = (ironMaterials * buffPercentage / ONE_HUNDRED_PERCENT)
 
     println("Бонусные кристаллы: $bonusCrystalMaterials")
     println("Бонусное железо: $bonusIronMaterials")
