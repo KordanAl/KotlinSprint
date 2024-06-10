7cd18570c4e56b3a2e31eaeb3e6a291b6210e77d
KorolevDA <valnamekda@gmail.com>
2024-06-10 20:39:07 +0300

    KS-2-4 bank deposit refactor

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,13 +2,15 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
+const val ONE_HUNDRED_PERCENT = 100
+
 fun main () {
 
     val depositAmount: Int = 70_000
     val depositYear: Byte = 20
     val interestRate: Double = 16.7
 
-    val totalDepositAmount = depositAmount * (1 + interestRate / 100).pow(depositYear.toDouble())
+    val totalDepositAmount = depositAmount * (1 + interestRate / ONE_HUNDRED_PERCENT).pow(depositYear.toDouble())
     val formatedTotalDepositAmount: String = String.format("%.3f", totalDepositAmount)
 
     println(formatedTotalDepositAmount)
