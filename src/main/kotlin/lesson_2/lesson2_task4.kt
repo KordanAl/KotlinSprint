c6f50bd6c232a5b368db66ff05d49b19d35782a1
KorolevDA <valnamekda@gmail.com>
2024-06-11 12:22:56 +0300

    KS-3-4 chess refact

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_2
 
-const val ONE_HUNDRED_PERCENT = 100
+const val HUNDRED_PERCENT = 100
 fun main () {
 
     val crystalMaterials = 7
     val ironMaterials = 11
     val buffPercentage = 20
 
-    val bonusCrystalMaterials = (crystalMaterials * buffPercentage / ONE_HUNDRED_PERCENT)
-    val bonusIronMaterials = (ironMaterials * buffPercentage / ONE_HUNDRED_PERCENT)
+    val bonusCrystalMaterials = (crystalMaterials * buffPercentage / HUNDRED_PERCENT)
+    val bonusIronMaterials = (ironMaterials * buffPercentage / HUNDRED_PERCENT)
 
     println("Бонусные кристаллы: $bonusCrystalMaterials")
     println("Бонусное железо: $bonusIronMaterials")
