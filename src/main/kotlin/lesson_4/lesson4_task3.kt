b0d15f6f0d4adf058b1bdbc338377914ccba03dd
KorolevDA <valnamekda@gmail.com>
2024-06-12 18:25:14 +0300

    KS-4-3 favorable conditions ( refactor )

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -2,10 +2,9 @@ package org.example.lesson_4
 
 // Критерии благоприятных условий включают:
 
-const val SUNNY_WEATHER = true
-const val OPEN_TENT = true
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
 const val AIR_HUMIDITY_PERCENTAGE: Int = 20
-const val CURRENT_TIME_OF_YEAR: String = "not winter"
 
 fun main() {
 
@@ -16,9 +15,10 @@ fun main() {
     val airHumidityNowPercentage: Int = 20
     val currentTimeOfYearNow = "winter"
 
-    val favorableConditions = listOf (SUNNY_WEATHER, OPEN_TENT, SUNNY_WEATHER, CURRENT_TIME_OF_YEAR)
-    val conditionsToday = listOf(sunnyWeatherNow, openTentForNow, airHumidityNowPercentage, currentTimeOfYearNow)
-
-    println("Благоприятные ли условия сейчас для роста бобовых? ${conditionsToday == favorableConditions}")
+    val favorableConditions: Boolean = (sunnyWeatherNow == IS_SUNNY)
+            && (openTentForNow == IS_AWNING_OPEN)
+            && (airHumidityNowPercentage == AIR_HUMIDITY_PERCENTAGE)
+            && (currentTimeOfYearNow != "winter")
+    println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 
 }
