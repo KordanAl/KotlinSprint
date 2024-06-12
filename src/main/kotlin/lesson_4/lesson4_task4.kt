9a3deaefc1fac91150dd7c3f7fe497e0b9a423c2
KorolevDA <valnamekda@gmail.com>
2024-06-12 17:48:47 +0300

    KS-4-4 training program ( refactor )

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -4,14 +4,13 @@ fun main() {
 
     val dayOfTraining = 5
 
-    val armAndAbsExercise = dayOfTraining % 2 != 0
-    val legAndBackExercises = dayOfTraining % 2 == 0
+    val isEven: (Int) -> Boolean = { dayOfTraining % 2 == 0 }
 
     println("""
-        Упражнения для рук:    $armAndAbsExercise
-        Упражнения для ног:    $legAndBackExercises
-        Упражнения для спины:   $legAndBackExercises
-        Упражнения для пресса: $armAndAbsExercise
+        Упражнения для рук:    ${!isEven(dayOfTraining)}
+        Упражнения для ног:    ${isEven(dayOfTraining)}
+        Упражнения для спины:   ${isEven(dayOfTraining)}
+        Упражнения для пресса: ${!isEven(dayOfTraining)}
     """.trimIndent())
 
 }
