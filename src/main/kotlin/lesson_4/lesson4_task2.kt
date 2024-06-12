3a46cc870918353dabeeffe65c2d94ed7049afc0
KorolevDA <valnamekda@gmail.com>
2024-06-12 22:52:03 +0300

    KS-4-2 average cargo (refactor 4)

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -2,9 +2,9 @@ package org.example.lesson_4
 
 const val MIN_WEIGHT_KG: Byte = 35
 const val MAX_WEIGHT_KG: Byte = 100
-const val MAX_VOLUME_LITER:Byte = 100
+const val MAX_VOLUME_LITER: Byte = 100
 
-fun main () {
+fun main() {
 
     val oneCargoKg = 20
     val oneCargoLiter = 80
@@ -13,17 +13,21 @@ fun main () {
     val twoCargoLiter = 100
 
     val oneAverageCargo = ((MIN_WEIGHT_KG > oneCargoKg)
-            && (oneCargoKg <=  MAX_WEIGHT_KG)
+            && (oneCargoKg <= MAX_WEIGHT_KG)
             && (oneCargoLiter < MAX_VOLUME_LITER))
 
     val twoAverageCargo = ((MIN_WEIGHT_KG > twoCargoKg)
-            && (twoCargoKg <=  MAX_WEIGHT_KG)
+            && (twoCargoKg <= MAX_WEIGHT_KG)
             && (twoCargoLiter < MAX_VOLUME_LITER))
 
-    println("Груз с весом $oneCargoKg кг и объемом $oneCargoLiter л соответствует категории 'Average':" +
-            " $oneAverageCargo")
+    println(
+        "Груз с весом $oneCargoKg кг и объемом $oneCargoLiter л соответствует категории 'Average':" +
+                " $oneAverageCargo"
+    )
 
-    println("Груз с весом $twoCargoKg кг и объемом $twoCargoLiter л соответствует категории 'Average':" +
-            " $twoAverageCargo")
+    println(
+        "Груз с весом $twoCargoKg кг и объемом $twoCargoLiter л соответствует категории 'Average':" +
+                " $twoAverageCargo"
+    )
 
 }
