9be44cbb82b67744f1d0e1a2dac181a22d2f2d31
KorolevDA <valnamekda@gmail.com>
2024-06-16 19:38:43 +0300

    KS-6-3  new timer

---
+++ src/main/kotlin/lesson_6/lesson6_task3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_6
+
+fun main() {
+
+    println("Сколько секунд необходимо засечь?")
+    var userSeconds: Int = readln().toInt()
+
+    val originalSeconds = userSeconds
+
+    while (userSeconds >= 0) {
+        println("Осталось секунд: ${userSeconds--}")
+        Thread.sleep(1000)
+    }
+    println("Время вышло")
+
+}
