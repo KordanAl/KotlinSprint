bf364f26bee8fdded68c63853ed5e791e993b00f
KorolevDA <valnamekda@gmail.com>
2024-06-16 17:42:06 +0300

    KS-6-1 user registration

---
+++ src/main/kotlin/lesson_6/lesson6_task1.kt
@@ -0,0 +1,42 @@
+package org.example.lesson_6
+
+fun main() {
+    var registration: Boolean = false
+    var login: String = ""
+    var password: String = ""
+
+    while (!registration) {
+        println("Введите логин, чтобы зарегестрироваться:")
+        login = readln()
+
+        println("Введите пароль, чтобы зарегестрироваться:")
+        Thread.sleep(1000)
+        password = readln()
+
+        println("Регистрация завершена.")
+        Thread.sleep(1000)
+        registration = true
+    }
+
+    var loggedIn: Boolean = false
+    var inputUsername: String = ""
+    var inputPassword: String = ""
+
+    while (!loggedIn) {
+        println("\nВведите ваш логин: ")
+        inputUsername = readln()
+
+        println("Введите ваш пароль: ")
+        Thread.sleep(1000)
+        inputPassword = readln()
+
+        if (inputUsername == login && inputPassword == password) {
+            println("\nАвторизация прошла успешно")
+            loggedIn = true
+        } else {
+            println("Логин или пароль введены , повторите попытку!")
+            Thread.sleep(1000)
+            continue
+        }
+    }
+}
