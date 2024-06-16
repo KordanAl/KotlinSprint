dddf5e66984a7fa342a7830f17fa5f552ee63012
KorolevDA <valnamekda@gmail.com>
2024-06-16 16:32:26 +0300

    KS-5-3 lottery (refactor)

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -11,19 +11,15 @@ fun main() {
         """.trimIndent(),
     )
 
-    val number1 = readLine()?.toInt()
+    val number1 = readln().toInt()
 
     println("Введите второе число:")
 
-    val number2 = readLine()?.toInt()
+    val number2 = readln().toInt()
 
-    if (number1 == ONE_NUMBER && number2 == TWO_NUMBER) {
+    if (number1 == ONE_NUMBER && number2 == TWO_NUMBER || number2 == ONE_NUMBER && number1 == TWO_NUMBER) {
         println("Поздравляем! Вы выиграли главный приз!")
-    } else if (number2 == ONE_NUMBER && number1 == TWO_NUMBER) {
-        println("Поздравляем! Вы выиграли главный приз!")
-    } else if (number1 != ONE_NUMBER && number2 == TWO_NUMBER) {
-        println("Поздравляем! Вы выиграли утешительный приз!")
-    } else if (number1 == ONE_NUMBER && number2 != TWO_NUMBER) {
+    } else if (number1 != ONE_NUMBER && number2 == TWO_NUMBER || number1 == ONE_NUMBER && number2 != TWO_NUMBER) {
         println("Поздравляем! Вы выиграли утешительный приз!")
     } else {
         println("Неудача!")
