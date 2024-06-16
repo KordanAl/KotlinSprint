4c830a6485eb446f89cbb90cb5874b25894fb69a
KorolevDA <valnamekda@gmail.com>
2024-06-16 08:18:05 +0300

    KS-5-3 lottery (refactor)

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -4,7 +4,6 @@ const val ONE_NUMBER = 10
 const val TWO_NUMBER = 35
 
 fun main() {
-
     println(
         """
         Чтобы выйграть в лотерреию нужно угадать два числа от 0 до 42.
@@ -22,6 +21,10 @@ fun main() {
         println("Поздравляем! Вы выиграли главный приз!")
     } else if (number2 == ONE_NUMBER && number1 == TWO_NUMBER) {
         println("Поздравляем! Вы выиграли главный приз!")
+    } else if (number1 != ONE_NUMBER && number2 == TWO_NUMBER) {
+        println("Поздравляем! Вы выиграли утешительный приз!")
+    } else if (number1 == ONE_NUMBER && number2 != TWO_NUMBER) {
+        println("Поздравляем! Вы выиграли утешительный приз!")
     } else {
         println("Неудача!")
     }
