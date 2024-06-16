b14e2e200014f1db315c657ecd921a26b0148e19
KorolevDA <valnamekda@gmail.com>
2024-06-16 07:00:36 +0300

    KS-5-3 lottery

---
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -0,0 +1,30 @@
+package org.example.lesson_5
+
+const val ONE_NUMBER = 10
+const val TWO_NUMBER = 35
+
+fun main() {
+
+    println(
+        """
+        Чтобы выйграть в лотерреию нужно угадать два числа от 0 до 42.
+        Введите первое число:
+        """.trimIndent(),
+    )
+
+    val number1 = readLine()?.toInt()
+
+    println("Введите второе число:")
+
+    val number2 = readLine()?.toInt()
+
+    if (number1 == ONE_NUMBER && number2 == TWO_NUMBER) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if (number2 == ONE_NUMBER && number1 == TWO_NUMBER) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else {
+        println("Неудача!")
+    }
+
+    println("Числа которые нужны были для победы: $ONE_NUMBER и $TWO_NUMBER.")
+}
