46af644eecc9b36c67778ee9368d2660c30dbeb8
KorolevDA <valnamekda@gmail.com>
2024-06-17 13:15:06 +0300

    KS-6-3 timer(refactor 2)

--- src/main/kotlin/lesson_6/lesson6_task3.kt
+++ src/main/kotlin/lesson_6/lesson6_task3.kt
@@ -5,8 +5,6 @@ fun main() {
     println("Сколько секунд необходимо засечь?")
     var userSeconds: Int = readln().toInt()
 
-    val originalSeconds = userSeconds
-
     while (userSeconds > 0) {
         println("Осталось секунд: ${userSeconds--}")
         Thread.sleep(1000)
