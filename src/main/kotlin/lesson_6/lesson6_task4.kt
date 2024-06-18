16c1a5e4d13218cd1640ed17f3418e6bca8197a2
KorolevDA <valnamekda@gmail.com>
2024-06-18 10:25:45 +0300

    KS-6-4 lottery game (refactor)

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -8,16 +8,15 @@ fun main() {
     var numberOfAttempts: Byte = 5
     val randomNumber = (MIN_NUMBER..MAX_NUMBER).random()
 
-    var correctNumber: Boolean = false
     var userNumber: Int?
 
-    while (!correctNumber && numberOfAttempts >= 0) {
+    while (numberOfAttempts >= 0) {
         println("Угадайте число от 1 до 9: ")
         userNumber = readln().toInt()
 
         if (userNumber == randomNumber) {
             println("Это была великолепная игра!")
-            correctNumber = true
+            return
         } else {
             println("Неверно, осталось попыток: ${numberOfAttempts--}")
         }
