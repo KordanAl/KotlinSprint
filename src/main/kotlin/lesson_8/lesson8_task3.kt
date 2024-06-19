5a1f6408894bc984847988e8fbdf79a717e94bd5
KordanAl <valnamekda@gmail.com>
2024-06-19 20:48:51 +0300

    KS-8-3 alternative search ingredient (refactor)

--- src/main/kotlin/lesson_8/lesson8_task3.kt
+++ src/main/kotlin/lesson_8/lesson8_task3.kt
@@ -11,11 +11,9 @@ fun main() {
     val arrayOfIngredients = arrayOf("Яйцо", "Помидор", "Зелень", "Соль", "Перец")
 
     println("Какой ингридиент вы бы хотели найти?")
-    val searchIngredient = readln()
+    val searchIngredient = readln().replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
 
-    val foundIngredient: Boolean = arrayOfIngredients.any { it.equals(searchIngredient, ignoreCase = true) }
-
-    if (foundIngredient) println("Ингредиент $searchIngredient в рецепте есть.")
+    if (searchIngredient in arrayOfIngredients) println("Ингредиент $searchIngredient в рецепте есть.")
     else println("Такого ингредиента в рецепте нет.")
 
 }
