202580239cb0c691b3ccf1bb0c4b155cbd215a63
KordanAl <valnamekda@gmail.com>
2024-06-20 11:32:44 +0300

    KS-9-4 creating a list of ingredients (refactor 2)

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -1,30 +1,23 @@
 package org.example.lesson_9
 
-const val QUANTITY_OF_INGREDIENTS = 5
-
 fun main() {
 
     println("Введите 5 названий ингредиентов блюда через запятую и пробел: ")
 
-    var input: String
     var ingredients: MutableList<String>
+    var inputIngredient: String
 
     do {
-        input = readln()
+        inputIngredient = readln()
+        ingredients = inputIngredient.split(", ").toMutableList()
 
-        ingredients =
-            input
-                .split(", ")
-                .map { all -> all.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() } }
-                .toMutableList()
-
-        if (ingredients.size != QUANTITY_OF_INGREDIENTS) {
+        if (ingredients.size < 5) {
             println("Ошибка! Введите ровно 5 ингредиентов, разделяя их запятыми и пробелом:")
         } else {
             println("Вы ввели следующие ингредиенты: ")
             break
         }
-    } while (ingredients.size != QUANTITY_OF_INGREDIENTS)
+    } while (ingredients.size < 5)
 
     ingredients = ingredients.sorted().toMutableList()
 
