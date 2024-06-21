126844249f1b197010548d83413429751755f7ee
KordanAl <valnamekda@gmail.com>
2024-06-21 18:20:22 +0300

    KS-9-5 creating a list of ingredients without repetitions (refactor 5)

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -1,22 +1,24 @@
 package org.example.lesson_9
 
 fun main() {
-
     println("Введите 5 названий ингредиентов, которые не повторяются.")
 
-    val ingredients: MutableList<String> = mutableListOf()
+    val ingredients = mutableSetOf<String>()
     var inputIngredient: String
 
     while (ingredients.size < 5) {
         print("Введите название ингредиента ${ingredients.size + 1}: ")
         inputIngredient = readln().lowercase()
+        ingredients += inputIngredient
 
-        if (inputIngredient.isBlank() || ingredients.contains(inputIngredient)) {
+//     Я ЗАПУТАЛСЯ, НЕ ПОЙМУ ЧТО НУЖНО
+
+        if (inputIngredient.isBlank()) {
             println("Ошибка! Вы не ввели название или ингридиент повторяется. Пожалуйста, попробуйте снова.")
             continue
         }
         ingredients.add(inputIngredient)
     }
-    println("Ингредиенты: ${ingredients.sorted().joinToString().replaceFirstChar { it.titlecase() }}.")
 
+    println("Ингредиенты: ${ingredients.sorted().joinToString().replaceFirstChar { it.titlecase() }}.")
 }
