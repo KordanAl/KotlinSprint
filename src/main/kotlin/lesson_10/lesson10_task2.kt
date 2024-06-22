862d5a613fa05eda54d6c2f088da0b26acbb411b
KordanAl <valnamekda@gmail.com>
2024-06-22 05:15:11 +0300

    KS-10-2 login or password length (refactor)

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -7,8 +7,10 @@ fun main() {
     println("Введите пароль, не менее 4 символов): ")
     val password: String = readln()
 
-    if (!validateInput(login) || !validateInput(password)) println("Логин или пароль недостаточно длинные")
-    else println("Регистрация успешна! Добро пожаловать!")
+    if (!validateInput(login) || !validateInput(password))
+        println("Логин или пароль недостаточно длинные")
+    else
+        println("Регистрация успешна! Добро пожаловать!")
 }
 
 fun validateInput(input: String) = input.length >= 4
