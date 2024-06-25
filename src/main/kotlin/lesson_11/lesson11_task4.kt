751262b2d73ed006dd1a4c2ff0c7553627a11a19
KordanAl <valnamekda@gmail.com>
2024-06-25 10:46:08 +0300

    KS-11-4-essences for recipes

---
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -0,0 +1,41 @@
+package org.example.lesson_11
+
+//- Категория рецепта
+//- Рецепт
+//- Ингредиент
+
+class Categories(
+
+    val id: Int,
+    val name: String,
+    val image: String,
+    val description: String,
+    val favorites: Boolean,
+
+) {
+
+}
+
+class Recipe(
+
+    val name: String,
+    val image: String,
+    val portions: Int,
+    val ingredientName: String,
+    val idOfIngredient: Int,
+    val amountOfIngredient: Int,
+    val favorites: Boolean,
+
+) {
+
+}
+
+class Ingredient(
+
+    val name: String,
+    val idOfIngredient: Int,
+    val totalAmountOfIngredient: Int,
+
+) {
+
+}
