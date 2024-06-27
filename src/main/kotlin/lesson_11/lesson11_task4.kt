063b2b756f455610d0b45a4a8fc2be044efa019c
KordanAl <valnamekda@gmail.com>
2024-06-27 16:01:05 +0300

    KS-11-4-essences for recipes (refactor)

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -5,36 +5,26 @@ package org.example.lesson_11
 //- Ингредиент
 
 class Categories(
-
     val id: Int,
     val name: String,
     val image: String,
     val description: String,
     val favorites: Boolean,
-
-) {
-
-}
+    val recipes: List<Recipe>
+)
 
 class Recipe(
-
     val name: String,
     val image: String,
     val portions: Int,
-    val ingredientName: String,
-    val idOfIngredient: Int,
     val amountOfIngredient: Int,
     val favorites: Boolean,
-
-) {
-
-}
+    val ingredients: List<Ingredient>,
+)
 
 class Ingredient(
-
+    val id: Int,
     val name: String,
-    val idOfIngredient: Int,
-
-) {
-
-}
+    val amount: Int,
+    val unit: String
+)
