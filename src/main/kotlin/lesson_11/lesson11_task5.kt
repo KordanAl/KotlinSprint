ba1a740f19e4dcfb464f66c1e0b2cab45362643b
KordanAl <valnamekda@gmail.com>
2024-06-28 08:24:55 +0300

    KS-11-5 forum (refactor 2)

--- src/main/kotlin/lesson_11/lesson11_task5.kt
+++ src/main/kotlin/lesson_11/lesson11_task5.kt
@@ -1,50 +1,43 @@
 package org.example.lesson_11
 
-class Forum {
-    class ForumUser(
-        val userId: Int,
-        val userName: String,
-    )
+class ForumUser(val userId: Int, val userName: String)
+class ForumMessage(val authorId: Int, val message: String)
 
-    class ForumMessage(
-        val authorId: Int,
-        val message: String,
-    )
+object Forum {
+    private val users: MutableList<ForumUser> = mutableListOf()
+    private val messages: MutableList<ForumMessage> = mutableListOf()
+    private var nextUserId: Int = 1
 
-    companion object {
-        private val users: MutableList<ForumUser> = mutableListOf()
-        private val messages: MutableList<ForumMessage> = mutableListOf()
-        private var nextUserId: Int = 1
+    fun createNewUser(userName: String): ForumUser {
+        val newUser = ForumUser(userId = nextUserId++, userName = userName)
+        users.add(newUser)
+        return newUser
+    }
 
-        fun createNewUser(userName: String): ForumUser {
-            val newUser = ForumUser(userId = nextUserId++, userName = userName)
-            users.add(newUser)
-            return newUser
+    fun createNewMessage(
+        authorId: Int,
+        message: String,
+    ): ForumMessage? {
+        val userIds = users.map { it.userId }
+        return if (authorId in userIds) {
+            val newMessage = ForumMessage(authorId, message)
+            messages.add(newMessage)
+            newMessage
+        } else {
+            println("Пользователь с идентификатором $authorId не существует.")
+            null
         }
+    }
 
-        fun createNewMessage(authorId: Int, message: String): ForumMessage? {
-            val userIds = users.map { it.userId }
-            return if (authorId in userIds) {
-                val newMessage = ForumMessage(authorId, message)
-                messages.add(newMessage)
-                newMessage
-            } else {
-                println("Пользователь с идентификатором $authorId не существует.")
-                null
-            }
-        }
-
-        fun printThread() {
-            for (message in messages) {
-                val author = users.find { it.userId == message.authorId }
-                println("${author?.userName}: ${message.message}")
-            }
+    fun printThread() {
+        for (message in messages) {
+            val author = users.find { it.userId == message.authorId }
+            println("${author?.userName}: ${message.message}")
         }
     }
 }
 
 fun main() {
-
     // Создание пользователей
     val user1 = Forum.createNewUser(userName = "Данила")
     val user2 = Forum.createNewUser(userName = "Влад")
