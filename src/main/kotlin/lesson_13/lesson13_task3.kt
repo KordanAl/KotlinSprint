7bbebd67b268c6278b29c826a42ca4e40610b45f
KordanAl <valnamekda@gmail.com>
2024-06-29 21:33:32 +0300

    KS-13-3 contact phone2

---
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -0,0 +1,24 @@
+package org.example.lesson_13
+
+class ContactPhone2(
+    private val name: String,
+    private val number: Long,
+    private val nameCompany: String? = null,
+) {
+    init {
+        if (nameCompany != null) {
+            println(nameCompany)
+        }
+    }
+}
+
+fun main() {
+    val phoneBook =
+        mutableListOf(
+            ContactPhone2("Ростислав", 89123456789, "Reddit"),
+            ContactPhone2("Данила", 89231231231),
+            ContactPhone2("Ян", 89119876543),
+            ContactPhone2("Влад", 89123456788, "null"),
+            ContactPhone2("Иван", 89112345678, "Google"),
+        )
+}
