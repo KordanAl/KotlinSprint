ca78f139062721466be18493260621c849f0bdae
KordanAl <valnamekda@gmail.com>
2024-07-02 07:03:02 +0300

    KS-13-1 contact phone (refactor)

--- src/main/kotlin/lesson_13/lesson13_task1.kt
+++ src/main/kotlin/lesson_13/lesson13_task1.kt
@@ -3,15 +3,5 @@ package org.example.lesson_13
 class ContactPhone(
     val name: String,
     val number: Long,
-) {
-
-    var nameCompany: String? = null
-
-    constructor(
-        name: String,
-        number: Long,
-        _nameCompany: String?,
-    ) : this(name, number) {
-        nameCompany = _nameCompany
-    }
-}
+    val nameCompany: String?,
+)
