443778ea75ba478fb59903e66ffacf35cedef2cd
KordanAl <valnamekda@gmail.com>
2024-07-03 10:30:05 +0300

    KS-13-5 contact phone4 (refactor)

--- src/main/kotlin/lesson_13/lesson13_task5.kt
+++ src/main/kotlin/lesson_13/lesson13_task5.kt
@@ -1,37 +1,34 @@
 package org.example.lesson_13
 
 class ContactPhone4(
-    private val name: String,
-    private val number: Long,
-    private val nameCompany: String? = null
+    val name: String,
+    val number: Long,
+    val nameCompany: String? = null,
 ) {
     companion object {
-        fun fromInput(name: String, numberInput: String, nameCompany: String? = null): ContactPhone4? {
-            val number = numberInput.toLongOrNull()
-            return if (number != null) {
-                ContactPhone4(name, number, nameCompany)
-            } else {
-                println("Ошибка: NumberFormatException для контакта '$name'.")
+        fun fromInput(
+            name: String,
+            number: String,
+            nameCompany: String? = null,
+        ): ContactPhone4? =
+            try {
+                ContactPhone4(name, number.toLong(), nameCompany)
+            } catch (e: NumberFormatException) {
+                println("Ошибка: ${e.message} для контакта '$name'.")
                 null
             }
-        }
     }
 
-    init {
-        println(toString())
-    }
-
-    override fun toString(): String {
-        return "$name, $number, ${nameCompany ?: "null"}"
-    }
+    override fun toString(): String = "$name, $number, ${nameCompany ?: "не указано"}"
 }
 
 fun main() {
-    val phoneBook = mutableListOf(
-        ContactPhone4.fromInput("Ростислав", "894552324", "Reddit"),
-        ContactPhone4.fromInput("Данила", "89231231231"),
-        ContactPhone4.fromInput("Ян", "это мой номер"),
-        ContactPhone4.fromInput("Влад", "89456546546", "null"),
-        ContactPhone4.fromInput("Иван", "89112345678", "Google")
-    )
+    val phoneBook =
+        mutableListOf(
+            ContactPhone4.fromInput("Ростислав", "894552324", "Reddit"),
+            ContactPhone4.fromInput("Данила", "89231231231"),
+            ContactPhone4.fromInput("Ян", "пусто поле"),
+            ContactPhone4.fromInput("Влад", "89456546546", "DNS"),
+            ContactPhone4.fromInput("Иван", "89112345678", "Google"),
+        )
 }
