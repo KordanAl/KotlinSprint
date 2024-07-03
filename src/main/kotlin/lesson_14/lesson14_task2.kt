e480b15fa1906e4ee14fa188936e6709ff365e76
KordanAl <valnamekda@gmail.com>
2024-07-03 15:17:26 +0300

    KS-14-2 ships (refactor2)

--- src/main/kotlin/lesson_14/lesson14_task2.kt
+++ src/main/kotlin/lesson_14/lesson14_task2.kt
@@ -1,27 +1,15 @@
 package org.example.lesson_14
 
-import java.text.NumberFormat
-import java.util.*
-
 open class Liner2(
     val name: String = "King",
     val speed: Int = 100,
     val liftingCapacity: Int = 1_000_000,
     val passengerCapacity: Int = 100_000,
-    val loadingMethod: String = "выдвигает горизонтальный трап со шкафута",
     val crushIce: Boolean = false,
-
 ) {
-    fun displayInformation2() {
-        val numberFormat = NumberFormat.getNumberInstance(Locale.forLanguageTag("ru"))
 
-        println("Информация о корабле: $name")
-        println("Скорость: $speed км/ч")
-        println("Грузоподъемность: ${numberFormat.format(liftingCapacity)} кг")
-        println("Вместимость экипажа: ${numberFormat.format(passengerCapacity)} человек")
-        println("Способ погрузки: $loadingMethod")
-        if (crushIce) println("В наличии имеется: 'Ледокол' ")
-        println()
+    open fun load() {
+        println("Лайнер: '$name' - выдвигает горизонтальный трап со шкафута для погрузки.")
     }
 }
 
@@ -30,27 +18,33 @@ class Cargo2(
     speed: Int = 50,
     liftingCapacity: Int = 2_000_000,
     passengerCapacity: Int = 50_000,
-    loadingMethod: String = "активирует погрузочный кран",
-) : Liner2(name, speed, liftingCapacity, passengerCapacity, loadingMethod)
+    crushIce: Boolean = false
+) : Liner2(name, speed, liftingCapacity, passengerCapacity) {
+
+    override fun load() {
+        println("Грузовой корабль: '$name' - активирует погрузочный кран для погрузки.")
+    }
+}
 
 class Icebreaker2(
     name: String = "Snowflake",
     speed: Int = 40,
     liftingCapacity: Int = 1_000_000,
     passengerCapacity: Int = 25_000,
-    loadingMethod: String = "открывает ворота со стороны кормы",
     crushIce: Boolean = true,
-) : Liner2(name, speed, liftingCapacity, passengerCapacity, loadingMethod, crushIce)
+) : Liner2(name, speed, liftingCapacity, passengerCapacity,) {
 
+    override fun load() {
+        println("Ледокол: '$name' - открывает ворота со стороны кормы для погрузки.")
+    }
+}
 
 fun main() {
-
     val liner = Liner2()
     val cargo = Cargo2()
     val iceBreaker = Icebreaker2()
 
-    liner.displayInformation2()
-    cargo.displayInformation2()
-    iceBreaker.displayInformation2()
-
+    liner.load()
+    cargo.load()
+    iceBreaker.load()
 }
