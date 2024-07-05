f5d5848fabaab56d8be360b911c4993fbd6d51cd
KordanAl <valnamekda@gmail.com>
2024-07-05 06:17:03 +0300

    KS-13-5 contact phone4 (refactor3)

--- src/main/kotlin/lesson_13/lesson13_task5.kt
+++ src/main/kotlin/lesson_13/lesson13_task5.kt
@@ -1,34 +1,35 @@
 package org.example.lesson_13
 
-class ContactPhone4(
+class ContactPhone5(
     val name: String,
-    val number: Long,
+    val number: String,
     val nameCompany: String? = null,
 ) {
-    companion object {
-        fun fromInput(
-            name: String,
-            number: String,
-            nameCompany: String? = null,
-        ): ContactPhone4? =
-            try {
-                ContactPhone4(name, number.toLong(), nameCompany)
-            } catch (e: NumberFormatException) {
-                println("Ошибка: ${e::class.simpleName} для контакта '$name'.")
-                null
-            }
-    }
-
     override fun toString(): String = "$name, $number, ${nameCompany ?: "не указано"}"
+}
+// все перепробовал, не получается
+fun fromInput(
+    name: String,
+    number: String,
+    nameCompany: String? = null,
+): ContactPhone5 {
+    return try {
+        val num = number.toLongOrNull() ?
+        ContactPhone5(name, num.toString(), nameCompany)
+    } catch (e: NumberFormatException) {
+        println("Ошибка: ${e::class.simpleName} для контакта '$name'.")
+        ContactPhone5(name, "0", nameCompany)
+    }
 }
 
 fun main() {
-    val phoneBook =
-        mutableListOf(
-            ContactPhone4.fromInput("Ростислав", "894552324", "Reddit"),
-            ContactPhone4.fromInput("Данила", "89231231231"),
-            ContactPhone4.fromInput("Ян", "пусто поле"),
-            ContactPhone4.fromInput("Влад", "89456546546", "DNS"),
-            ContactPhone4.fromInput("Иван", "89112345678", "Google"),
-        )
+    val phoneBook = mutableListOf(
+        fromInput("Ростислав", "894552324", "Reddit"),
+        fromInput("Данила", "89231231231"),
+        fromInput("Ян", ""),
+        fromInput("Влад", "89456546546", "DNS"),
+        fromInput("Иван", "89112345678", "Google")
+    )
+
+    phoneBook.forEach { println(it) }
 }
