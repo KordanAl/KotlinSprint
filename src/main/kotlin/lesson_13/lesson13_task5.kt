a976e4f6213523421def5c3d490fa53fc9a2a654
KordanAl <valnamekda@gmail.com>
2024-07-05 06:49:19 +0300

    KS-13-5 contact phone4 (refactor4)

--- src/main/kotlin/lesson_13/lesson13_task5.kt
+++ src/main/kotlin/lesson_13/lesson13_task5.kt
@@ -1,35 +1,23 @@
 package org.example.lesson_13
 
-class ContactPhone5(
+class ContactPhone4(
     val name: String,
     val number: String,
     val nameCompany: String? = null,
 ) {
     override fun toString(): String = "$name, $number, ${nameCompany ?: "не указано"}"
 }
-// все перепробовал, не получается
-fun fromInput(
-    name: String,
-    number: String,
-    nameCompany: String? = null,
-): ContactPhone5 {
+
+fun fromInput(name: String, number: String, nameCompany: String?): ContactPhone4 {
     return try {
-        val num = number.toLongOrNull() ?
-        ContactPhone5(name, num.toString(), nameCompany)
-    } catch (e: NumberFormatException) {
-        println("Ошибка: ${e::class.simpleName} для контакта '$name'.")
-        ContactPhone5(name, "0", nameCompany)
+        val num = number.toLong()
+        ContactPhone4(name, num.toString(), nameCompany)
+    } catch (e: Exception) {
+        ContactPhone4(name, "ошибка: ${e::class.simpleName}")
     }
 }
 
 fun main() {
-    val phoneBook = mutableListOf(
-        fromInput("Ростислав", "894552324", "Reddit"),
-        fromInput("Данила", "89231231231"),
-        fromInput("Ян", ""),
-        fromInput("Влад", "89456546546", "DNS"),
-        fromInput("Иван", "89112345678", "Google")
-    )
-
-    phoneBook.forEach { println(it) }
+    val contact = fromInput("Ян", "123ABC", "")
+    println(contact)
 }
