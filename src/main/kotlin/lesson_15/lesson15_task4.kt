728b47c31c0da067047dd0641189d46d41ce3517
KordanAl <valnamekda@gmail.com>
2024-07-06 19:34:24 +0300

    KS-15-4 music shop ( refactor )

--- src/main/kotlin/lesson_15/lesson15_task4.kt
+++ src/main/kotlin/lesson_15/lesson15_task4.kt
@@ -1,33 +1,47 @@
 package org.example.lesson_15
 
 interface Searchable {
-    fun search()
+    fun searchAccessory()
 }
 
-abstract class Product(val name: String, val quantityInStock: Int) : Searchable
+abstract class Product(
+    val name: String,
+    val quantityInStock: Int,
+)
 
-class Instrument(name: String, quantityInStock: Int) : Product(name, quantityInStock) {
-    override fun search() {
+class Instrument(
+    name: String,
+    quantityInStock: Int,
+    val accessoryList: List<Accessory>,
+) : Product(name, quantityInStock),
+    Searchable {
+    override fun searchAccessory() {
         println("Выполняется поиск комплектующих для инструмента $name")
         for (i in 1..3) {
             println(i)
             Thread.sleep(1000)
         }
-        println("Найдены следующие комплектующие ....")
+        if (accessoryList.isEmpty()) {
+            println("Комплектующие не найдены.")
+        } else {
+            println("Найдены следующие комплектующие:")
+            for (accessory in accessoryList) {
+                println("- ${accessory.name}, в наличии: ${accessory.quantityInStock}")
+            }
+        }
     }
 }
 
-class Accessory(name: String, quantityInStock: Int) : Product(name, quantityInStock) {
-    override fun search() {
-        TODO("Not yet implemented")
-    }
-
-}
+class Accessory(
+    name: String,
+    quantityInStock: Int,
+) : Product(name, quantityInStock)
 
 fun main() {
-    val guitar = Instrument("Гитара", 10)
-
     val guitarStrings = Accessory("Струны для гитары", 50)
+    val guitarPick = Accessory("Медиатор", 100)
 
-    guitar.search()
+    val guitarAccessory = Instrument("Гитара", 10, listOf(guitarStrings, guitarPick))
+
+    guitarAccessory.searchAccessory()
 }
