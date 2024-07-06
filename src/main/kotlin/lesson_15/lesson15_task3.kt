cba109e40285fefe28cb9b953d8d78e73270b9c2
KordanAl <valnamekda@gmail.com>
2024-07-06 17:58:00 +0300

    KS-15-3 forum user (refactor)

--- src/main/kotlin/lesson_15/lesson15_task3.kt
+++ src/main/kotlin/lesson_15/lesson15_task3.kt
@@ -1,75 +1,48 @@
 package org.example.lesson_15
 
-abstract class ForumUser {
-
-    abstract val name: String
-    abstract val adminPrivilege: Boolean
-    abstract val text: String
-    abstract fun loginToTheForum()
-    abstract fun wright()
-    abstract fun read()
-
+abstract class UserForum(
+    val userName: String
+) {
+    abstract fun readForum()
+    abstract fun writeMessage(message: String)
 }
 
-class SimpleUser(
-    override val name: String,
-    override val adminPrivilege: Boolean = false,
-    override val text: String = ""
-) : ForumUser() {
-    override fun loginToTheForum() {
-        println("Вошел на форум")
+class SimpleUser(userName: String) : UserForum(userName) {
+    override fun readForum() {
+        println("$userName читает форум.")
     }
 
-    override fun wright() {
-        println("$name напечатал сообщение: $text")
-    }
-
-    override fun read() {
-        println("$name: читает...")
+    override fun writeMessage(message: String) {
+        println("$userName написал сообщение: $message")
     }
 }
 
-class AdminUser(
-    override val name: String,
-    override val adminPrivilege: Boolean = true,
-    override val text: String = ""
-) : ForumUser() {
-    override fun loginToTheForum() {
-        println("Вошел на форум")
+class AdminUser(userName: String) : UserForum(userName) {
+    override fun readForum() {
+        println("$userName читает форум.")
     }
 
-    override fun wright() {
-        println("$name напечатал сообщение: $text")
+    override fun writeMessage(message: String) {
+        println("$userName написал сообщение: $message")
     }
 
-    override fun read() {
-        println("$name читает...")
+    fun deleteMessage(message: String) {
+        println("$userName удалил сообщение: $message")
     }
 
-    fun deleteMessage(text: String) {
-        println("Удалил сообщение: $text")
-    }
-
-    fun deleteUser(name: String) {
-        println("Удалил пользователя: $name")
+    fun deleteUser(userName: String) {
+        println("${this.userName} удалил пользователя : $userName")
     }
 }
 
 fun main() {
+    val user1 = SimpleUser("Danila")
+    user1.readForum()
+    user1.writeMessage("Java отстой")
 
-    val user = SimpleUser("Данила", text = "Подскажите пожалуйста, как вывести список в консоль")
-    val user1 = SimpleUser("ПсевдоГуру", text = "Нужно нажать alt + f4")
-
-    var userA = AdminUser("Георгий")
-
-    user.wright()
-    user1.wright()
-    user.read()
-    userA.read()
-    userA.deleteMessage(user1.text)
-    userA.deleteUser(user1.name)
-
-    userA = AdminUser("Георгий", text = "Не слушай его, я сейчас тебе все расскажу!")
-
-    userA.wright()
+    val admin1 = AdminUser("Vadim.admin")
+    admin1.readForum()
+    admin1.writeMessage("Пожалуйста, соблюдайте правила форума.")
+    admin1.deleteMessage("'Java отстой'")
+    admin1.deleteUser(user1.userName)
 }
