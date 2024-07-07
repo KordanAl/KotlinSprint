60b1ef15aaec0a36260ad924aaaa25abfc5e0aa8
KordanAl <valnamekda@gmail.com>
2024-07-07 19:12:57 +0300

    KS-16-2 circle

---
+++ src/main/kotlin/lesson_16/lesson16_task2.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_16
+
+import kotlin.math.pow
+
+class Circle(private val radius: Double) {
+
+    companion object {
+        private const val PI: Double = 3.14
+    }
+
+    fun getAreaOfCircle(): Double = PI * radius.pow(2)
+
+    fun getCircumference(): Double = 2 * PI * radius
+}
+
+fun main() {
+    val circle = Circle(12.0)
+
+    println("The circumference is: ${circle.getCircumference()} mm")
+    println("The area of the circle is ${circle.getAreaOfCircle()} mm")
+}
