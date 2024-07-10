ebba57851ab7526f4a91f2873f31ba9f58473f62
KordanAl <valnamekda@gmail.com>
2024-07-10 13:52:35 +0300

    KS-17-3 folders

---
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -0,0 +1,34 @@
+package org.example.lesson_17
+
+class Folder(
+    private val _name: String,
+    private var _numberOfFiles: Int,
+    private var security: Boolean = false,
+) {
+    val name: String
+        get() =
+            if (security) {
+                "Hidden folder."
+            } else {
+                _name
+            }
+
+    val numberOfFiles: Int
+        get() =
+            if (security) {
+                0
+            } else {
+                _numberOfFiles
+            }
+}
+
+fun main() {
+    val folder1 = Folder("KotlinSprint", 85)
+    val folder2 = Folder("KotlinHacks", 874354, true)
+
+    println(folder1.name)
+    println("Number of files: ${folder1.numberOfFiles}")
+    println()
+    println(folder2.name)
+    println("Number of files: ${folder2.numberOfFiles}")
+}
