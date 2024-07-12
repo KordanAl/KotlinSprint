eeb9a7ae3984b2fa1e831f875b5e0dbe20e1679f
KordanAl <valnamekda@gmail.com>
2024-07-12 13:02:31 +0300

    KS-17-5 user data1 (refactor)

--- src/main/kotlin/lesson_17/lesson17_task5.kt
+++ src/main/kotlin/lesson_17/lesson17_task5.kt
@@ -1,18 +1,13 @@
 package org.example.lesson_17
 
 class UserData1(
-    private var _login: String,
+    private val _login: String,
     private val _password: String,
 ) {
-    // создал данный метод для проверки, что все работает корректно
-    fun getUserDataInfo() {
-        println("User login: $_login, user password: $_password")
-    }
 
     var login = _login
         set(value) {
             field = value
-            _login = value
             println("The login change is successful! New login: $value")
         }
     var password: String
@@ -30,7 +25,4 @@ fun main() {
 
     println("Current user password: ${user1.password}")
     user1.password = "ZASDaqweqwdqw1234"
-
-    user1.getUserDataInfo()
-
 }
