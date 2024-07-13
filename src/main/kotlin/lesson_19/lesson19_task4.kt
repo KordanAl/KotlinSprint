d391d695d61944747dba52ccab5db37da324ede4
KordanAl <valnamekda@gmail.com>
2024-07-13 17:52:36 +0300

    KS-19-4 tank shooting

--- src/main/kotlin/lesson_19/lesson19_task4.kt
+++ src/main/kotlin/lesson_19/lesson19_task4.kt
@@ -1,2 +1,48 @@
 package org.example.lesson_19
 
+enum class TypePatron {
+    BLUE,
+    GREEN,
+    RED;
+
+    fun getDamageTypePatron(): Int {
+        return when (this) {
+            BLUE -> 5
+            GREEN -> 10
+            RED -> 20
+        }
+    }
+
+    fun getNameTypePatron(): String {
+        return when (this) {
+            BLUE -> "Синий"
+            GREEN -> "Зеленый"
+            RED -> "Красный"
+        }
+    }
+}
+
+class Tank(private val name: String) {
+
+    fun takeShot(patron: TypePatron) {
+        println("$name произвел выстрел и нанес врагу ${patron.getDamageTypePatron()} урона")
+    }
+
+    fun runLoadNewPatron(patron: TypePatron) {
+        println("$name воорудил орудие типом патронов: ${patron.getNameTypePatron()}")
+    }
+}
+
+fun main() {
+
+    val tank = Tank("T-34")
+
+    val patrons = listOf(TypePatron.BLUE, TypePatron.GREEN, TypePatron.RED)
+
+    for (patron in patrons) {
+        tank.runLoadNewPatron(patron)
+        tank.takeShot(patron)
+        Thread.sleep(1000)
+
+    }
+}
