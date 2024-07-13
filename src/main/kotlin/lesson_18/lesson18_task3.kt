b113d5ccc6404b416b0b4d088518bfb45ede63f9
KordanAl <valnamekda@gmail.com>
2024-07-13 15:10:29 +0300

    KS-18-3 animal (refactor)

--- src/main/kotlin/lesson_18/lesson18_task3.kt
+++ src/main/kotlin/lesson_18/lesson18_task3.kt
@@ -1,25 +1,26 @@
 package org.example.lesson_18
 
-abstract class Animal(val name: String) {
+abstract class Animal() {
+    abstract val name: String
 
     abstract fun eat(): String
 
     fun sleep(): String = "$name -> sleeps"
 }
 
-class Fox(name: String) : Animal(name) {
+class Fox(override val name: String) : Animal() {
     override fun eat(): String {
         return "$name -> eats berries"
     }
 }
 
-class Dog(name: String) : Animal(name) {
+class Dog(override val name: String) : Animal() {
     override fun eat(): String {
         return "$name -> eats bones"
     }
 }
 
-class Cat(name: String) : Animal(name) {
+class Cat(override val name: String) : Animal() {
     override fun eat(): String {
         return "$name -> eats fish"
     }
