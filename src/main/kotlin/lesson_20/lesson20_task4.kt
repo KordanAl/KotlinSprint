2bf469614aac43a04312b741b3ad6afa17ef2e1f
KordanAl <valnamekda@gmail.com>
2024-07-14 15:45:08 +0300

    KS-20-4 list lambdas

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -1,2 +1,18 @@
 package org.example.lesson_20
 
+fun main() {
+    val elements = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+
+    val listLambdas = elements.map { element ->
+        { println("Нажат элемент $element") }
+    }
+
+    val printEvenElements: (List<() -> Unit>) -> Unit = { lambdas ->
+        for (i in lambdas.indices) {
+            if (elements[i].toInt() % 2 == 0) {
+                lambdas[i]()
+            }
+        }
+    }
+    printEvenElements(listLambdas)
+}
