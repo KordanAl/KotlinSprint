701e335cfffd6a220a8ae1046e1e75f8f8d3f311
KordanAl <valnamekda@gmail.com>
2024-07-14 09:34:37 +0300

    KS-20-1 splash screen (refactor)

--- src/main/kotlin/lesson_20/lesson20_task1.kt
+++ src/main/kotlin/lesson_20/lesson20_task1.kt
@@ -5,7 +5,7 @@ fun main() {
     val splashScreen: (String) -> String = { username: String ->
         "С наступающим Новым Годом, $username!"
     }
-    val username = splashScreen("Иван")
-    println(username)
+    val userName = splashScreen("Иван")
+    println(userName)
 }
 
