8bc05112618a143c3e37c5b7b04528d22c864984
KordanAl <valnamekda@gmail.com>
2024-07-14 09:52:50 +0300

    KS-18-5 draw figures (refactor)

--- src/main/kotlin/lesson_18/lesson18_task5.kt
+++ src/main/kotlin/lesson_18/lesson18_task5.kt
@@ -1,49 +1,39 @@
 package org.example.lesson_18
 
-open class Figure {
-    open fun draw() = ""
-}
-
-class Circle<T : Number>(
-    private val x: T,
-    private val y: T,
-) : Figure() {
-    override fun draw(): String {
-        return "Рисуем круг с координатами ($x, $y)"
-    }
-}
-
-class Square<T : Number>(
-    private val x: T,
-    private val y: T,
-) : Figure() {
-    override fun draw(): String {
-        return "Рисуем квадрат с координатами ($x, $y)"
-    }
-}
-
-class Point<T : Number>(
-    private val x: T,
-    private val y: T,
-) : Figure() {
-    override fun draw(): String {
-        return "Рисуем точку с координатами ($x, $y)"
-    }
-}
+class Circle
+class Square
+class Point
 
 class Screen {
-    fun showAllFiguresDraw(figures: List<Figure>) {
-        figures.forEach { println(it.draw()) }
+    fun draw(x: Int, y: Int, figure: Circle) {
+        println("Рисуем круг с координатами ($x, $y)")
+    }
+
+    fun draw(x: Float, y: Float, figure: Circle) {
+        println("Рисуем круг с координатами ($x, $y)")
+    }
+
+    fun draw(x: Int, y: Int, figure: Square) {
+        println("Рисуем квадрат с координатами ($x, $y)")
+    }
+
+    fun draw(x: Float, y: Float, figure: Square) {
+        println("Рисуем квадрат с координатами ($x, $y)")
+    }
+
+    fun draw(x: Int, y: Int, figure: Point) {
+        println("Рисуем точку с координатами ($x, $y)")
+    }
+
+    fun draw(x: Float, y: Float, figure: Point) {
+        println("Рисуем точку с координатами ($x, $y)")
     }
 }
 
 fun main() {
-    val circle: Figure = Circle(10.5f, 20)
-    val square: Figure = Square(15.5, 25.5)
-    val point: Figure = Point(5, 5)
-
     val screen = Screen()
 
-    val listFigures = listOf(circle, square, point)
-    screen.showAllFiguresDraw(listFigures)
+    screen.draw(10.5f, 20.0f, Circle())
+    screen.draw(15.5f, 25.5f, Square())
+    screen.draw(5, 5, Point())
 }
