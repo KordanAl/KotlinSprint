aa1e600fffbcbc245d6721b13743e016d36500c0
KordanAl <valnamekda@gmail.com>
2024-07-16 15:36:16 +0300

    KS-21-4 add fun writeToLowercaseToBeginning

--- src/main/kotlin/lesson_21/lesson21_task4.kt
+++ src/main/kotlin/lesson_21/lesson21_task4.kt
@@ -1,2 +1,22 @@
 package org.example.lesson_21
 
+import java.io.File
+import java.util.*
+
+fun File.writeToLowercaseToBeginning(text: String) {
+    val resultText = text.lowercase(Locale.getDefault())
+    val originalText = this.readText()
+
+    this.writeText(resultText + "\n" + originalText)
+}
+
+fun main() {
+    val newTextFile: File = File("NewTextFile.txt")
+
+    newTextFile.createNewFile()
+    newTextFile.writeToLowercaseToBeginning("Привет, мир!")
+    newTextFile.writeToLowercaseToBeginning("Этот текст будет в начале")
+
+    println(newTextFile.readText())
+}
+
