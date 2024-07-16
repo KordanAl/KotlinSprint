4e733b16807fb54158d6db7f3ece167d2b88bfb4
KordanAl <valnamekda@gmail.com>
2024-07-16 13:39:51 +0300

    KS-21-2 evenNumbersSum

--- src/main/kotlin/lesson_21/lesson21_task2.kt
+++ src/main/kotlin/lesson_21/lesson21_task2.kt
@@ -1,2 +1,13 @@
 package org.example.lesson_21
 
+fun Collection<Int>.evenNumbersSum(): Int {
+    return this.filter { it % 2 == 0 }.sum()
+}
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 234, 851, 1117)
+    println("Сумма всех четных чисел в списке: ${numbers.evenNumbersSum()}")
+
+    val numbers1 = setOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 234, 851, 1117)
+    println("Сумма всех четных чисел в списке: ${numbers1.evenNumbersSum()}")
+}
