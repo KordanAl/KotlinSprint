7b660745e720df6b7f2e72db07ea0ef00abd31f3
KordanAl <valnamekda@gmail.com>
2024-07-17 14:15:23 +0300

    KS-22-2 outputting data in classes

--- src/main/kotlin/lesson_22/lesson22_task2.kt
+++ src/main/kotlin/lesson_22/lesson22_task2.kt
@@ -1,2 +1,16 @@
 package org.example.lesson_22
 
+class RegularBook1(val name: String, val author: String)
+
+data class DataBook1(val name: String, val author: String)
+
+fun main() {
+    val user1 = RegularBook1("Уличный кот по кличке боб", "Джеймс Боуэен")
+    val user2 = DataBook1("Бойцовский клуб", "Чак Поланик")
+
+    //Непонятные символы потому, что в обычном классе так выглядит строковый вывод по умолчанию toString() в принте
+    println(user1)
+    // В дата классе уже переопределена функция toString() для вывода данных.
+    println(user2)
+}
+
