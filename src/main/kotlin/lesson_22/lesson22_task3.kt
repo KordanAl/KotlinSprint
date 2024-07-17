00020ee6e8788e0c36aef2be2266a21515caf559
KordanAl <valnamekda@gmail.com>
2024-07-17 14:25:47 +0300

    KS-22-3 destructuring date class

--- src/main/kotlin/lesson_22/lesson22_task3.kt
+++ src/main/kotlin/lesson_22/lesson22_task3.kt
@@ -1,2 +1,13 @@
 package org.example.lesson_22
 
+data class Car(val name: String, val speed: Int, val year: Int)
+
+fun main() {
+
+    val car1 = Car("Aston Martin", 280, 2005)
+
+    val (name, speed, year) = car1
+
+    println("Марка авто: $name, скорость: $speed, год выпуска: $year")
+}
+
