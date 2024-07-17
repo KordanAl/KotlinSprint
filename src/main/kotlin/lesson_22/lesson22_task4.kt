5debba1d240f7a06e8937d68dbb82698994ae4c8
KordanAl <valnamekda@gmail.com>
2024-07-17 17:52:31 +0300

    KS-22-4 MainScreenViewModel

--- src/main/kotlin/lesson_22/lesson22_task4.kt
+++ src/main/kotlin/lesson_22/lesson22_task4.kt
@@ -1,2 +1,44 @@
 package org.example.lesson_22
 
+data class MainScreenState(
+    val data: String = "отуствуют",
+    val isLoading: Boolean = false,
+) {
+    override fun toString(): String {
+        return if (!isLoading) {
+            "Текущее состояние:[данные: $data, состояние загрузки: $isLoading]\n"
+        } else {
+            "Текущее состояние:[данные: в процессе загрузки, состояние загрузки: $isLoading]\n"
+        }
+    }
+}
+
+class MainScreenViewModel {
+
+    var mainScreenState = MainScreenState()
+
+    fun loadData() {
+        mainScreenState = mainScreenState.copy(isLoading = true)
+        println("Загрузка данных...")
+        print(mainScreenState)
+        Thread.sleep(3000)
+
+        val loadedData = "Погода сегодня: 28° тепла"
+        mainScreenState = mainScreenState.copy(data = loadedData, isLoading = false)
+        println("Загруженны данные: $loadedData \n")
+    }
+
+}
+
+fun main() {
+    val viewModel = MainScreenViewModel()
+
+    // Состояние: отсутствие данных
+    println(viewModel.mainScreenState)
+
+    // Загрузка данных
+    viewModel.loadData()
+
+    // Состояние: данные загружены
+    println(viewModel.mainScreenState)
+}
